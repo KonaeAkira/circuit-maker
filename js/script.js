8f8bc61f5e70b20c1641055ea22d6a55bffb3708
KonaeAkira <konae.akira@gmail.com>
2018-11-28 16:29:54 +0700

    It worksssss

--- js/script.js
+++ js/script.js
@@ -6,6 +6,56 @@ var modifyFlag = false;
 const gateWidth = 62;
 const gateHeight = 34;
 
+var scanner = setInterval(function(){
+	scan();
+}, 1);
+
+function probe(gateID){
+	if (gateID == 0) {
+		return false;
+	} else {
+		return $('#canvas .gate[gate-id="' + gateID + '"]').attr('this-state') == 'true';
+	}
+}
+
+function scan(){
+	$('#canvas .gate').each(function(index){
+		var type = $(this).attr('gate-type');
+		var ID = $(this).attr('gate-id');
+		var inputOne = $(this).attr('input-1');
+		var inputTwo = $(this).attr('input-2');
+		if (type == 'or'){
+			$(this).attr('next-state', probe(inputOne) | probe(inputTwo));
+		} else if (type == 'nor'){
+			$(this).attr('next-state', !probe(inputOne) & !probe(inputTwo));
+		} else if (type == 'and'){
+			$(this).attr('next-state', probe(inputOne) & probe(inputTwo));
+		} else if (type == 'nand'){
+			$(this).attr('next-state', !(probe(inputOne) & probe(inputTwo)));
+		} else if (type == 'xor'){
+			$(this).attr('next-state', probe(inputOne) ^ probe(inputTwo));
+		} else if (type == 'xnor'){
+			$(this).attr('next-state', !probe(inputOne) ^ probe(inputTwo));
+		} else if (type == 'not'){
+			$(this).attr('next-state', !probe(inputOne));
+		} else if (type == 'out'){
+			$(this).attr('next-state', probe(inputOne));
+		}
+	});
+	$('#canvas .gate').each(function(index){
+		if ($(this).attr('gate-type') != 'inp'){
+			$(this).attr('this-state', $(this).attr('next-state') == '1');
+		}
+	});
+	$('#canvas path').each(function(index){
+		if (probe($(this).attr('input'))){
+			$(this).addClass('active');
+		} else {
+			$(this).removeClass('active');
+		}
+	});
+}
+
 function selectType(caller){
 	$('.gate-container.selected').removeClass('selected').addClass('unselected');
 	$(caller).removeClass('unselected').addClass('selected');
